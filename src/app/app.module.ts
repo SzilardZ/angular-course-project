91f46f8527fe7e52f8e90ac8e455c2e958dea701
SzilardZ <szilard.zr@gmail.com>
2019-03-05 17:33:41 +0100

    refactoring: component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {ShoppingListComponent} from './shopping-list/shopping-list/shopping-list.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ShoppingListEditComponent} from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import {RecipeListComponent} from './recipe-book/recipe-list/recipe-list.component';
-import {RecipeItemComponent} from './recipe-book/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
+import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import {RecipesComponent} from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule
