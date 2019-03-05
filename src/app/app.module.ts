5420a2176d0d246e3d40bc30015c945d6ae1a82f
SzilardZ <szilard.zr@gmail.com>
2019-03-05 17:11:43 +0100

    recipe-detail component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {ShoppingListComponent} from './shopping-list/shopping-list/shopping-list
 import {ShoppingListEditComponent} from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import {RecipeListComponent} from './recipe-book/recipe-list/recipe-list.component';
 import {RecipeItemComponent} from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import {RecipeItemComponent} from './recipe-book/recipe-item/recipe-item.compone
     ShoppingListEditComponent,
     RecipeListComponent,
     RecipeItemComponent,
+    RecipeDetailComponent,
   ],
   imports: [
     BrowserModule
