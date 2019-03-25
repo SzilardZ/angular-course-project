7838222b35b11b078543a80f91260d3115b71b2c
SzilardZ <szilard.zr@gmail.com>
2019-03-25 14:30:25 +0100

    recipe service created

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ import {Recipe} from '../recipe.model';
 export class RecipeListComponent {
   recipes: Recipe[] = [
     new Recipe('bolognese', 'delicious food',
-      'https://cdn.pixabay.com/photo/2015/05/27/18/53/spaghetti-787048_960_720.jpg'),
+      'https://cdn.pixabay.com/photo/2014/02/16/13/32/spaghetti-bolognese-267289_960_720.jpg'),
     new Recipe('pizza', 'italian speciality',
       'https://upload.wikimedia.org/wikipedia/commons/d/d3/Supreme_pizza.jpg')
   ];
