97725c4231c02dacfefbd05594e512711f3cd0d4
SzilardZ <szilard.zr@gmail.com>
2019-04-30 12:04:43 +0200

    route parameters for recipes configured

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,14 +11,16 @@ export class RecipeService {
    selectedRecipe = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('bolognese',
+    new Recipe(
+      'bolognese',
       'delicious food',
       'https://www.maxpixel.net/static/photo/1x/Spaghetti-Bolognese-Eat-Food-Parmesan-Delicious-787048.jpg',
       [
         new Ingredient('cheese', 2),
         new Ingredient('tomato', 3),
       ]),
-    new Recipe('pizza', 'italian speciality',
+    new Recipe(
+    'pizza', 'italian speciality',
       'https://upload.wikimedia.org/wikipedia/commons/d/d3/Supreme_pizza.jpg',
       [
         new Ingredient('flour', 4),
@@ -37,5 +39,8 @@ export class RecipeService {
     this.shoppingListService.addIngredientList(ingredients);
   }
 
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
 
 }
